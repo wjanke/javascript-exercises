9466e06c178d67350b482b0ef652d4c4148ee079
William Janke <herscht@gmx.de>
2025-12-23 20:05:16 +0100

    solution to 11th javascript foundations assignment

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(temp_fahrenheit) {
+    temp_celsius=(temp_fahrenheit-32)*5/9;
+    return round(temp_celsius,1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp_celsius) {
+    temp_fahrenheit=(temp_celsius*9/5)+32;
+    return round(temp_fahrenheit,1);
 };
 
 // Do not edit below this line
