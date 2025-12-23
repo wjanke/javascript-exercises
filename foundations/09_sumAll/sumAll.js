e41a82c8ef9ad535fe5326d1a23a6adbc08c9277
William Janke <herscht@gmx.de>
2025-12-23 19:18:55 +0100

    solution to 9th javascript foundations assigment

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1<0 || num2<0) return "ERROR";
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    sum=0;
+    for (i = Math.min(num1,num2); i<=Math.max(num1,num2); i++) {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
