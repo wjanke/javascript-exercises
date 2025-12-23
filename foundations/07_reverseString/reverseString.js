9bf443bbe98b4415bede21525b648b65ed87f7f7
William Janke <herscht@gmx.de>
2025-12-23 18:58:53 +0100

    solution to seventh javascript foundations assignment
    simpler would be usage of .split("").reverse().join("")

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    reverse=""
+    for (i=str.length - 1;i >= 0;i--) {
+        reverse+=str.at(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
