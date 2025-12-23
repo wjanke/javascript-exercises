de445849a6c2b19b6d9cf67fe1a9e72ab710b27c
William Janke <herscht@gmx.de>
2025-12-23 18:46:08 +0100

    solution to sixth javascript foundations exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str1, num) {
+    if (num<0) return "ERROR";
+    returnstring="";
+    for (i=1; i<=num; i++){
+        returnstring+=str1;
+    }
+    return returnstring;
 };
 
 // Do not edit below this line
