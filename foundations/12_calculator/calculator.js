40f906e7318e8114a7cc6274cad8c71af92eec8e
William Janke <herscht@gmx.de>
2025-12-26 16:20:40 +0100

    solution to 12th javascript foundations assignment

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total,current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product,current) => product * current, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	fact=1;
+  for (i=1; i<=n; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
