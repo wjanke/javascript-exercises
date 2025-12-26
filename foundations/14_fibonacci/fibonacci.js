8bd1c364644e4c3602cfef805e49cdef5f614115
William Janke <herscht@gmx.de>
2025-12-26 16:46:59 +0100

    solution to 14th javascript foundatsions assignment

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  fibnum=parseInt(num);
+  if (fibnum == 0) return 0;
+  if (fibnum < 0) return "OOPS";
+  fibarr=[1,1];
+  len = fibarr.length;
+  while (fibarr.length < fibnum) {
+    fibarr.push(fibarr[len-1] + fibarr[len-2]);
+    len = fibarr.length;
+  };
+  return fibarr[len-1];
 };
 
 // Do not edit below this line
