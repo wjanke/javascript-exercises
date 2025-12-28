7a1c88c5a1765b0b262352c207283d866adf3178
William Janke <herscht@gmx.de>
2025-12-28 10:55:07 +0100

    solution for the 16th javascript foundations assignment

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+  //calc ages
+  const currentYear = parseInt(new Date().getFullYear());
+  oldest = 0;
+  oldestPerson = "Unknown"
+  for (let person of persons) {
+    if (person.yearOfDeath === undefined) {
+      ageYearMax = currentYear;
+    } else {
+      ageYearMax = person.yearOfDeath;
+    }
+    age = ageYearMax - person.yearOfBirth;
+    if (age > oldest) {
+      oldest = age;
+      oldestPerson = person;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
